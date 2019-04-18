2c00a0887f6edad0a46e6de616880291c1f91c1a
Cucheng821015 <wl02856836@gmail.com>
2019-04-18 18:05:28 +0800

    me更新

--- js/me.js
+++ js/me.js
@@ -1,21 +1,4 @@
 
-// (function (e) {
-//     var slice = Array.prototype.slice;
-//     var tabs = slice.call(e.getElementsByClassName('tabLi'));
-//     var panels = slice.call(e.getElementsByClassName('box'));
-//     tabs.find(function (tab) {
-//         tab.addEventListener('click', function () {
-//             //點擊這個tab時，先將其他tab隱藏
-//             panels.find(function (panel) {
-//                 panel.style.display = 'none';
-//             });
-//             e.querySelector(tab.getAttribute('target')).style.display = 'block';
-//         });
-//     });
-//     //預設顯示第一個tab
-//     tabs[0].click();
-// }(this.document));
-
 function jsTabs(evt, tabId) {
     var tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabs-panel");
