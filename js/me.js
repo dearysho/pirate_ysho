16de99601066a7e693ded7f5cdc964ad9765dbbb
Cucheng821015 <wl02856836@gmail.com>
2019-04-18 13:59:54 +0800

    雷達圖JS上傳

--- js/me.js
+++ js/me.js
@@ -1,30 +1,36 @@
-// 寶物道具頁籤
-// (function ($) {
-//     var tabs = $(".tabs li a");
 
-//     tabs.click(function () {
-//         var content = this.hash.replace('/', '');
-//         tabs.removeClass("active");
-//         $(this).addClass("active");
-//         $("#content").find('div').hide();
-//         $(content).fadeIn(200);
+// (function (e) {
+//     var slice = Array.prototype.slice;
+//     var tabs = slice.call(e.getElementsByClassName('tabLi'));
+//     var panels = slice.call(e.getElementsByClassName('box'));
+//     tabs.find(function (tab) {
+//         tab.addEventListener('click', function () {
+//             //點擊這個tab時，先將其他tab隱藏
+//             panels.find(function (panel) {
+//                 panel.style.display = 'none';
+//             });
+//             e.querySelector(tab.getAttribute('target')).style.display = 'block';
+//         });
 //     });
-// })(jQuery);
+//     //預設顯示第一個tab
+//     tabs[0].click();
+// }(this.document));
 
-(function (e) {
-    var slice = Array.prototype.slice;
-    var tabs = slice.call(e.getElementsByClassName('tabLi'));
-    var panels = slice.call(e.getElementsByClassName('box'));
-    //遍历每一个卡片tab标签，为其增加点击事件监听器
-    tabs.find(function (tab) {
-        tab.addEventListener('click', function () {
-            //当点击这个tab标签时，先将所有的面板隐藏
-            panels.find(function (panel) {
-                panel.style.display = 'none';
-            });
-            e.querySelector(tab.getAttribute('target')).style.display = 'block';
-        });
-    });
-    //初始化时默认点击一下第一个tab标签，用来给一个默认的现实面板。
-    tabs[0].click();
-}(this.document));
+function jsTabs(evt, tabId) {
+    var tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabs-panel");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].className = tabcontent[i].className.replace(" animated bounceIn", "");
+    }
+    tablinks = document.getElementsByClassName("tabs-menu");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tabs-menu-active", "");
+    }
+    var tab = document.getElementById(tabId);
+    tab.className += " animated bounceIn";
+    tab.style.display = "block";
+    evt.currentTarget.className += " tabs-menu-active";
+    return false;
+}
+
---
+++ js/radar.js
@@ -0,0 +1,110 @@
+// 建立4個var 為了之後要傳入radar(代號,代號,代號,代號)
+var str = 0;
+var int = 0;
+var lcu = 0;
+var agi = 0;
+
+function addSkill(e) {
+    // 1.get技能點數
+    // 2.把技能點數轉為number
+    var Points = parseInt(document.getElementById('points').innerText);
+    // alert(typeof Points);
+
+    // 3.判斷技能點數>=1時才能夠加技能(利用if-else)
+    // console.log(e.target.innerText);
+    if (Points >= 1) {
+        document.getElementById('points').innerText = Points - 1;
+        // 4.判斷哪個btn被點(利用e.target 與 if-else)
+        if (e.target.innerText == '力量') {
+            str += 5;
+        } else if (e.target.innerText == '智力') {
+            int += 5;
+        } else if (e.target.innerText == '幸運') {
+            lcu += 5;
+        } else if (e.target.innerText == '敏捷') {
+            agi += 5;
+        }
+        // 4個變數帶入radar( , , , )
+        radar(str, int, lcu, agi);
+    } else {
+        alert("技能點數不足");
+    }
+}
+
+function radar(str, int, lcu, agi) {
+    //定義變數
+    var chartRadarDOM;
+    var chartRadarData;
+    var chartRadarOptions;
+
+    //載入雷達圖
+    Chart.defaults.global.legend.display = false;
+    Chart.defaults.global.defaultFontColor = 'rgba(0,0,74, 1)'; //字的顏色
+    chartRadarDOM = document.getElementById("chartRadar");
+    chartRadarData;
+    chartRadarOptions =
+        {
+            scale:
+            {
+                ticks:
+                {
+                    fontSize: 16,
+                    beginAtZero: true,
+                    maxTicksLimit: 10,
+                    min: 0,
+                    max: 100
+                },
+                pointLabels:
+                {
+                    fontSize: 25,
+                    color: '#0044BB'
+                },
+                gridLines:
+                {
+                    color: '#009FCC'
+                }
+            }
+        };
+
+    console.log("---------Rader Data--------");
+    var graphData = new Array();
+    graphData.push(str);
+    graphData.push(int);
+    graphData.push(lcu);
+    graphData.push(agi);
+
+
+    console.log("--------Rader Create-------------");
+    console.log(graphData);
+
+    //CreateData
+    chartRadarData = {
+        labels: ['力量', '智力', '幸運', '敏捷'],
+        datasets: [{
+            label: "Skill Level",
+            backgroundColor: "rgba(17, 34, 51,0.8)",
+            borderColor: "rgba(63,63,74,.8)",
+            pointBackgroundColor: "rgba(63,63,74,1)",
+            pointBorderColor: "rgba(0,0,0,0)",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(0,0,0,0.3)",
+            pointBorderWidth: 5,
+            data: graphData
+        }]
+    };
+
+    //Draw
+    var chartRadar = new Chart(chartRadarDOM, {
+        type: 'radar',
+        data: chartRadarData,
+        options: chartRadarOptions
+    });
+}
+
+function init() {
+    radar(str, int, lcu, agi); //第一次呼叫radar,為了在loading頁面時radar出現
+    for (var i = 0; i <= 3; i++) {
+        document.getElementsByClassName('but')[i].onclick = addSkill;
+    }
+}
+window.onload = init;
