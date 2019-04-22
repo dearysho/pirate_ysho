a574216a901da65a33144e214d7f62ea18beb4ca
explorer717ed <explorer717ed@gmail.com>
2019-04-22 12:43:04 +0800

    修改RWD 還差的遠...

--- js/home.js
+++ js/home.js
@@ -75,103 +75,107 @@ $(document).ready(function () {
 
     //要開始移動地圖了喔
 
-    //第一屏:船放大到DIY
-    var mapToDIYTl = new TimelineMax();
-    mapToDIYTl.add(TweenMax.to('.homeMap',1,{scale:3,opacity:0,}))
-        .add(TweenMax.to('#shipArea',1,{scale:1,x:'50%',y:'-20%'}),'0')
-        .set($('.homeMap'),{css:{zIndex:-1}})
+// if (winWidth >=1024){
+//     //第一屏:船放大到DIY
+//     var mapToDIYTl = new TimelineMax();
+//     mapToDIYTl.add(TweenMax.to('.homeMap',1,{scale:3,opacity:0,}))
+//         .add(TweenMax.to('#shipArea',1,{scale:1,x:'50%',y:'-20%'}),'0')
+//         .set($('.homeMap'),{css:{zIndex:-1}})
 
-    var ctrl = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: '0'
-        }
-    });
+//     var ctrl = new ScrollMagic.Controller({
+//         globalSceneOptions: {
+//             triggerHook: '0'
+//         }
+//     });
 
-    new ScrollMagic.Scene({
-        duration:'95%',
-        // triggerElement:$('#homeDIY'),
-    })
-    .addIndicators({name:'homeDIY'})
-    .setTween(mapToDIYTl)
-    .addTo(ctrl);
+//     new ScrollMagic.Scene({
+//         duration:'95%',
+//         // triggerElement:$('#homeDIY'),
+//     })
+//     .addIndicators({name:'homeDIY'})
+//     .setTween(mapToDIYTl)
+//     .addTo(ctrl);
 
-    //第二幕:船移動到試煉場
-    var mapToGameTl = new TimelineMax();
-    mapToGameTl.add(TweenMax.to('.homeMap',1,{
-            scale:1.5,
-            opacity:1,
-            x:'15%',
-            y:'-20%',
-            onStart:function(){
-                mapToDIYTl.set('.homeMap',{className:'-=justHide'})
-            }
-        }),'0')
-        .add(TweenMax.to('#shipArea',1,{scale:0.5,x:'60%',y:'-20%'}),'0')
-        .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'1')
-        .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'1')
-        // .set('.homeMap',{className:"-=justHide"})
+//     //第二幕:船移動到試煉場
+//     var mapToGameTl = new TimelineMax();
+//     mapToGameTl.add(TweenMax.to('.homeMap',1,{
+//             scale:1.5,
+//             opacity:1,
+//             x:'15%',
+//             y:'-20%',
+//             onStart:function(){
+//                 mapToDIYTl.set('.homeMap',{className:'-=justHide'})
+//             }
+//         }),'0')
+//         .add(TweenMax.to('#shipArea',1,{scale:0.5,x:'60%',y:'-20%'}),'0')
+//         .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'1')
+//         .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'1')
+//         // .set('.homeMap',{className:"-=justHide"})
         
 
-    new ScrollMagic.Scene({
-        duration:'90%',
-        triggerElement:$('#homeDIY'),
-    })
-    .addIndicators({name:'game'})
-    .setTween(mapToGameTl)
-    .addTo(ctrl)
+//     new ScrollMagic.Scene({
+//         duration:'90%',
+//         triggerElement:$('#homeDIY'),
+//     })
+//     .addIndicators({name:'game'})
+//     .setTween(mapToGameTl)
+//     .addTo(ctrl)
 
-    // var shipToGame = new TimelineMax();
-    // shipToGame.add(TweenMax.to('#shipArea',1,{scale:0.3,x:'90%'}),'0')
-    //     .add(TweenMax.fromTo('#homeGameBanner',1,
-    //         {x:'100%',scale:2,opacity:0},
-    //         {x:'28%',scale:1,opacity:1}
-    //         ),'0')
-    //     // .add(TweenMax.to('#shipArea',1,{x:'100%'}))
+//     // var shipToGame = new TimelineMax();
+//     // shipToGame.add(TweenMax.to('#shipArea',1,{scale:0.3,x:'90%'}),'0')
+//     //     .add(TweenMax.fromTo('#homeGameBanner',1,
+//     //         {x:'100%',scale:2,opacity:0},
+//     //         {x:'28%',scale:1,opacity:1}
+//     //         ),'0')
+//     //     // .add(TweenMax.to('#shipArea',1,{x:'100%'}))
 
-    // new ScrollMagic.Scene({
-    //     duration:'60%',
-    //     triggerElement:$('#homeDIY'),
-    // })
-    // .setPin($('#homeGameBanner'))
-    // .addIndicators({name:'game'})
-    // .setTween(shipToGame)
-    // .addTo(ctrl)
+//     // new ScrollMagic.Scene({
+//     //     duration:'60%',
+//     //     triggerElement:$('#homeDIY'),
+//     // })
+//     // .setPin($('#homeGameBanner'))
+//     // .addIndicators({name:'game'})
+//     // .setTween(shipToGame)
+//     // .addTo(ctrl)
 
-    //第三幕：船到海上市集
-    var mapToMarketTl = new TimelineMax();
-    mapToMarketTl.add(TweenMax.to('.homeMap',1,{opacity:1,}),'0')
-        .add(TweenMax.to('#shipArea',1,{opacity:1}),'0')
-        .add(TweenMax.to('.homeMap',1,{x:'10%',y:'-70%'}),'0')
-        .add(TweenMax.to('.homeMap',1,{x:'5%',y:'-75%'}),'1')
-        .add(TweenMax.to('.homeMap',1,{x:'-20%',y:'-25%'}),'2')
-        .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'3')
-        .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'3')
-
-        
-
-    new ScrollMagic.Scene({
-        duration:'100%',
-        triggerElement:$('#homeGameGPS'),
-    })
-    .addIndicators({name:'market'})
-    .setTween(mapToMarketTl)
-    .addTo(ctrl)
-
-    //第四幕：船到情報酒館
-    var mapToBarTl = new TimelineMax();
-    mapToBarTl.add(TweenMax.to('.homeMap',1,{opacity:1,}),'0')
-        .add(TweenMax.to('#shipArea',1,{opacity:1}),'0')
-        .add(TweenMax.to('.homeMap',1,{x:'-35%'}),'1')
-        .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'2')
-        .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'2')
+//     //第三幕：船到海上市集
+//     var mapToMarketTl = new TimelineMax();
+//     mapToMarketTl.add(TweenMax.to('.homeMap',1,{opacity:1,}),'0')
+//         .add(TweenMax.to('#shipArea',1,{opacity:1}),'0')
+//         .add(TweenMax.to('.homeMap',1,{x:'10%',y:'-70%'}),'0')
+//         .add(TweenMax.to('.homeMap',1,{x:'5%',y:'-75%'}),'1')
+//         .add(TweenMax.to('.homeMap',1,{x:'-20%',y:'-25%'}),'2')
+//         .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'3')
+//         .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'3')
 
         
 
-    new ScrollMagic.Scene({
-        duration:'80%',
-        triggerElement:$('#homeMarketProdInfo'),
-    })
-    .addIndicators({name:'bar'})
-    .setTween(mapToBarTl)
-    .addTo(ctrl)
+//     new ScrollMagic.Scene({
+//         duration:'100%',
+//         triggerElement:$('#homeGameGPS'),
+//     })
+//     .addIndicators({name:'market'})
+//     .setTween(mapToMarketTl)
+//     .addTo(ctrl)
+
+//     //第四幕：船到情報酒館
+//     var mapToBarTl = new TimelineMax();
+//     mapToBarTl.add(TweenMax.to('.homeMap',1,{opacity:1,}),'0')
+//         .add(TweenMax.to('#shipArea',1,{opacity:1}),'0')
+//         .add(TweenMax.to('.homeMap',1,{x:'-35%'}),'1')
+//         .add(TweenMax.to('#shipArea',1,{opacity:0,delay:1}),'2')
+//         .add(TweenMax.to('.homeMap',1,{opacity:0,delay:1}),'2')
+
+        
+
+//     new ScrollMagic.Scene({
+//         duration:'80%',
+//         triggerElement:$('#homeMarketProdInfo'),
+//     })
+//     .addIndicators({name:'bar'})
+//     .setTween(mapToBarTl)
+//     .addTo(ctrl)
+
+// }
+
 });
